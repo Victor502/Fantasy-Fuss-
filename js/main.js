41fff4033fff61fd9e758b99ef5e8d071c9e3136
Victor Horton <victorahorton@yahoo.com>
2017-09-06 14:19:38 -0400

    fixed player stats page and Navbars

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-
 function myFunction() {
-  // Declare variables 
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
+    // Declare variables   var input, filter, table, tr, td, i;
+    const input = document.getElementById("myInput");
+    const filter = input.value.toUpperCase();
+    const table = document.getElementById("myTable");
+    const tr = table.getElementsByTagName("tr");
+    const th = table.getElementsByTagName("th");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
+
+
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+                
+            } else {
+                tr[i].style.display = "none";
+            }
+            
+        }
+    }
 }
 
 
 $('.carousel').carousel();
-
